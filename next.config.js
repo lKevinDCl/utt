064b20cde2c4c28478227ee69b095dae80e32e0b
Kevin Diego <kevindiego2608@gmail.com>
2023-07-10 08:57:33 -0600

    Files updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    ...nextConfig,
+    env: {
+
+    API_KEY: "AIzaSyBUZQWgHtcmzcKAreCJWXwaVt3NfV_UbwY",
+    AUTH_DOMAIN: "utt-dsm-pw.firebaseapp.com",
+    PROJECT_ID: "utt-dsm-pw",
+    STORAGE_BUCKET: "utt-dsm-pw.appspot.com",
+    MESSAGING_SENDER_ID: "730674998910",
+    APP_ID: "1:730674998910:web:c1bdfb7ef1da25ec16f22f",
+    MEASURMENT_ID: "G-NRVFVJ4MGG"
+    
+        },
+    }
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      soy home
+      soy profile
     </div>
       )
 }
